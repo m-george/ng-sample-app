538bda5c870636f615988c07833c1de37e37df00
George Michalios <gmihalios@yahoo.com>
2022-04-22 00:00:26 +0300

    Register new components & routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { AppHeaderComponent } from './app-header/app-header.component';
+import { AppHeaderNavComponent } from './app-header-nav/app-header-nav.component';
+import { HomeComponent } from './home/home.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginFormComponent,
+    AppHeaderComponent,
+    AppHeaderNavComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
