b55a2ed18a9e275dce763ed15206ed3b4a8488da
George Michalios <gmihalios@yahoo.com>
2022-04-21 23:59:49 +0300

    Add app header nav component

---
+++ src/app/app-header-nav/app-header-nav.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header-nav',
+  templateUrl: './app-header-nav.component.html',
+  styleUrls: ['./app-header-nav.component.scss']
+})
+export class AppHeaderNavComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
